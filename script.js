d536178414fb2953cf7fa1e754c2bd045fcde0a6
oop final project <l250664@lhr.nu.edu.pk>
2026-07-20 18:20:10 +0500

    Select DOM elements and initialize score variables

---
+++ script.js
@@ -0,0 +1,9 @@
+const rockButton = document.getElementById("rock");
+const paperButton = document.getElementById("paper");
+const scissorsButton = document.getElementById("scissors");
+const resultText = document.getElementById("result");
+const playerScoreText = document.getElementById("playerScore");
+const computerScoreText = document.getElementById("computerScore");
+
+let playerScore = 0;
+let computerScore = 0;
